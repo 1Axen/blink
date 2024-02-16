212e16d50c932e1f94c14fc4d31df774c8f88db4
DoomIsEternal <65947351+DoomIsEternal@users.noreply.github.com>
2024-02-17 01:16:57 +0200

    New build process with support for aftman

--- build/build.bat
+++ build/build.bat
@@ -1,9 +1,12 @@
 @echo Off
-SET WINRAR="C:\Program Files\WinRAR\Rar.exe"
+SET ZIP="C:\Program Files\7-Zip\7z.exe"
+SET VERSION="0.5.0"
 
 echo Clearing release folder
 rmdir /s/q "../Blink"
 mkdir "../Blink"
+
+lune run version.luau %VERSION%
 
 echo Bundling source code
 darklua process ../src/init.luau ./Bundled.luau
@@ -11,17 +14,17 @@ darklua process ../src/init.luau ./Bundled.luau
 echo Building standalone executable
 lune build ./Bundled.luau
 ren "./Bundled.exe" "blink.exe"
-move "./blink.exe" "../release/"
 
 echo Compiling bytecode
 lune run compile.luau
 
-echo Cloning init file
-echo f | xcopy "./init.luau" "../Blink/init.luau" /f /v /s
-
 echo Zipping files
-%WINRAR% a "../release/bytecode.rar" "../Blink/*"
+%ZIP% a "../release/blink-%VERSION%-windows-x86_64.zip" "blink.exe" > nul
+%ZIP% a "../release/bytecode.zip" "Bytecode.txt" > nul
+%ZIP% a "../release/bytecode.zip" "init.luau" > nul
 
+del "./*.exe"
+del "./Bytecode.txt"
 rmdir /s/q "../Blink" 
 
 pause
