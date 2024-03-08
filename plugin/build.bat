f95876e8828a0174068130475458bbff4968478e
DoomIsEternal <65947351+DoomIsEternal@users.noreply.github.com>
2024-03-08 11:08:56 +0200

    Force error widget to always be visible

--- plugin/build.bat
+++ plugin/build.bat
@@ -3,7 +3,7 @@
 mkdir "./bundle"
 copy ".\src\Error.rbxmx" ".\bundle\Error.rbxmx"
 copy ".\src\Widget.rbxmx" ".\bundle\Widget.rbxmx"
-REM copy "..\build\.darklua.json" ".\.darklua.json"
+copy "..\build\.darklua.json" ".\.darklua.json"
 
 darklua process "./src/init.server.luau" "./bundle/init.server.lua"
 rojo build bundle.project.json --output "%LocalAppData%\Roblox\Plugins\Blink.rbxmx"
