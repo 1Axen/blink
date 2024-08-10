6f6df4e6a6fd33adcecbb1e797e379e3281cb465
Axen <axen@frombloxware.com>
2024-08-10 22:24:23 +0300

    Weird merge with docs branch

---
+++ docs/theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Blink</span>,
+  project: {
+    link: 'https://github.com/1Axen/blink',
+  },
+  docsRepositoryBase: 'https://github.com/1Axen/blink',
+  footer: {
+    text: '© 2024 Blink',
+  },
+}
+
+export default config
